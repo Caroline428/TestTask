312caa5820873580be38a9f6b616adde448c7ae9
krol1999 <sasagricenko916@gmail.com>
2022-01-21 00:04:23 +0300

    modal window

--- js/main.js
+++ js/main.js
@@ -5,74 +5,139 @@ const photoUrl = 'https://json.medrating.org/photos?albumId=';
 const btnCatalog = document.querySelector('.btn-catalog')
 const btnFavorites = document.querySelector('.btn-favourites')
 const mainContent = document.querySelector('.main__content')
-const mainButton = document.querySelector('.main__button')
+const mainContainer = document.querySelector('.main__container')
+
+let favouritePhoto = []
+start()
+
+btnCatalog.addEventListener('click', getContent)
+btnFavorites.addEventListener('click', getFavouritesContent)
+
+mainContent.addEventListener('click', function (event) {
+    let target = event.target
+    let id = target.getAttribute('id')
 
 
- start()
+    if (target.classList.contains('item_title') && !target.classList.contains('open') ) {
+        let box = target.parentElement
+        box.insertAdjacentHTML('afterend', `<div class="item__content item__content_${id}"> </div>`)
+        getAlbums(id, target)
+        target.classList.add('open')
+    }
 
+    else if (target.classList.contains('item_title') && target.classList.contains('open') ) {
+        let deleteTarget = document.querySelector(`.item__content_${id}`)
+        deleteTarget.remove()
+        target.classList.remove('open')
+    }
 
+    else if (target.classList.contains('album_title') && !target.classList.contains('open')) {
+        let box = target.parentElement
+        box.insertAdjacentHTML('afterend', `<div class="album__content album__content_${id}"> </div>`)
+        getPhoto(id, target)
+        target.classList.add('open')
+    }
 
+    else if (target.classList.contains('album_title') && target.classList.contains('open') ) {
+        let deleteTarget = document.querySelector(`.album__content_${id}`)
+        deleteTarget.remove()
+        target.classList.remove('open')
+    }
+    else if (target.classList.contains('star') && target.classList.contains('empty')) {
+        addFavorite(target.parentElement)
+        target.classList.remove('empty')
+        target.classList.add('active')
+        console.log(target.parentElement)
+    }
+    else if (target.classList.contains('star') && target.classList.contains('active')) {
+        removeFavorite(target.parentElement)
+        target.classList.remove('active')
+        target.classList.add('empty')
+    }
+    else if (target.classList.contains('photo')) {
+        displayModal(target)
+    }
 
+})
 
-function start() {
-    getContent()
-    btnCatalog.addEventListener('click', getContent)
-    btnFavorites.addEventListener('click', getFavourites)
+mainContainer.addEventListener('click', function (event) {
+        let target = event.target
+        if (target.classList.contains('modal')) {
+            removeModal(target)
+            console.log('шо такое')
+        }
+    })
+
+async function start() {
+     getContent()
+
+    getFavourites()
+}
+////////////////////
+function displayModal(target) {
+    let url = target.parentElement.getAttribute('href')
+    mainContent.insertAdjacentHTML('afterend', `
+    <div class="modal">
+        <div class="modal__img">
+           <img src="${url}">
+         </div>
+    </div>
+    `)
 }
 
-function getContent() {
+function removeModal(target) {
+    target.remove()
+}
+///////////////////
+
+async function getContent() {
     if (btnFavorites.classList.contains('btn_active')) {
         btnFavorites.classList.remove('btn_active')
         mainContent.innerHTML = ''
     }
     btnCatalog.classList.add('btn_active')
+
     getUsers()
-    mainContent.addEventListener('click', function (event) {
-        let target = event.target
-        let id = target.getAttribute('id')
 
-
-        if (target.classList.contains('item_title') && !target.classList.contains('open') ) {
-            let box = document.querySelector('.item__box')
-            box.insertAdjacentHTML('afterend', `<div class="item__content item__content_${id}"> </div>`)
-            getAlbums(id, target)
-            target.classList.add('open')
-        }
-
-        else if (target.classList.contains('item_title') && target.classList.contains('open') ) {
-            let deleteTarget = document.querySelector(`.item__content_${id}`)
-            deleteTarget.remove()
-            target.classList.remove('open')
-        }
-
-        else if (target.classList.contains('album_title') && !target.classList.contains('open')) {
-            let box = document.querySelector('.album__box')
-            box.insertAdjacentHTML('afterend', `<div class="album__content album__content_${id}"> </div>`)
-            getPhoto(id, target)
-            target.classList.add('open')
-        }
-
-        else if (target.classList.contains('album_title') && target.classList.contains('open') ) {
-            let deleteTarget = document.querySelector(`.album__content_${id}`)
-            deleteTarget.remove()
-            target.classList.remove('open')
-        }
-    })
 }
 
-function getFavourites() {
+function getFavouritesContent() {
     btnCatalog.classList.remove('btn_active')
     btnFavorites.classList.add('btn_active')
     mainContent.innerHTML = ''
 
 }
 
+//////////////////////////////////////////
+function addFavorite(target) {
+    let id = target.getAttribute('id')
+    console.log(target.firstElementChild)
+    let url = target.firstElementChild.getAttribute('src')
+    localStorage.setItem(id,url)
+
+}
+
+function removeFavorite(target) {
+    let id = target.getAttribute('id')
+    localStorage.removeItem(id)
+
+}
+
+function getFavourites() {
+    for (let i=0; i<localStorage.length; i++) {
+        favouritePhoto[i] = localStorage.key(i)
+        console.log(localStorage.key(i))
+    }
+}
+
+
+
 
 //////////////////////////////////////////
 async function getUsers() {
-
+    displayPreLoader(mainContent)
     try {
-        displayPreLoader(mainContent)
+
         let users = await fetch(usersUrl)
             .then(response => response.json())
 
@@ -94,9 +159,9 @@ async function getUsers() {
 }
 
 async function getAlbums(userID, target) {
-
+    displayPreLoader(target)
     try {
-         displayPreLoader(target)
+
         let mainItem = document.querySelector(`.item__content_${userID}`)
         let albums = await fetch(albumsUrl + userID)
             .then(response => response.json())
@@ -120,9 +185,9 @@ async function getAlbums(userID, target) {
 }
 
 async function getPhoto(albumID, target) {
-
+    displayPreLoader(target)
     try {
-         displayPreLoader(target)
+
         let mainItem = document.querySelector(`.album__content_${albumID}`)
         let photo = await fetch(photoUrl + albumID)
             .then(response => response.json())
@@ -183,13 +248,16 @@ function displayAlbums(item, mainItem) {
 
 function displayPhoto(item, mainItem) {
     let id = item.id
-
     let thumbnailUrl = item.thumbnailUrl
+    let url = item.url
+    let title =  item.title
     mainItem.insertAdjacentHTML('beforeend',
         `
-        <div class="album__photo">
-            <img src="${thumbnailUrl}">
-            <button class="star empty"></button>
+        <div class="album__photo album__photo_${id}" id="${id}">
+            <a href="${url}" onclick="return false">
+                <img class="photo" src="${thumbnailUrl}" title="${title}">
+            </a>
+            <button class="star ${favouritePhoto.includes(id.toString()) ? 'active' : 'empty'}"></button>
          </div>
         `
         )
@@ -232,14 +300,7 @@ function removeError() {
 
 ///////////////////////////////
 
-// function clickCatalog(event) {
-//     if (mainContent.hasAttribute('hidden')) {
-//         mainContent.removeAttribute('hidden')
-//     }
-//     else {
-//         mainContent.setAttribute('hidden','true')
-//     }
-// }
+
 
 
 
