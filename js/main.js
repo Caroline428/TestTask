19161c4a197490dbc326568834d9dad753586d4d
krol1999 <sasagricenko916@gmail.com>
2022-01-20 06:12:25 +0300

    started developing favorites

--- js/main.js
+++ js/main.js
@@ -3,18 +3,74 @@ const albumsUrl = 'https://json.medrating.org/albums?userId=';
 const photoUrl = 'https://json.medrating.org/photos?albumId=';
 
 const btnCatalog = document.querySelector('.btn-catalog')
+const btnFavorites = document.querySelector('.btn-favourites')
 const mainContent = document.querySelector('.main__content')
+const mainButton = document.querySelector('.main__button')
+
+
+ start()
 
 
 
 
 
+function start() {
+    getContent()
+    btnCatalog.addEventListener('click', getContent)
+    btnFavorites.addEventListener('click', getFavourites)
+}
 
-getUsers()
+function getContent() {
+    if (btnFavorites.classList.contains('btn_active')) {
+        btnFavorites.classList.remove('btn_active')
+        mainContent.innerHTML = ''
+    }
+    btnCatalog.classList.add('btn_active')
+    getUsers()
+    mainContent.addEventListener('click', function (event) {
+        let target = event.target
+        let id = target.getAttribute('id')
+
+
+        if (target.classList.contains('item_title') && !target.classList.contains('open') ) {
+            let box = document.querySelector('.item__box')
+            box.insertAdjacentHTML('afterend', `<div class="item__content item__content_${id}"> </div>`)
+            getAlbums(id, target)
+            target.classList.add('open')
+        }
+
+        else if (target.classList.contains('item_title') && target.classList.contains('open') ) {
+            let deleteTarget = document.querySelector(`.item__content_${id}`)
+            deleteTarget.remove()
+            target.classList.remove('open')
+        }
+
+        else if (target.classList.contains('album_title') && !target.classList.contains('open')) {
+            let box = document.querySelector('.album__box')
+            box.insertAdjacentHTML('afterend', `<div class="album__content album__content_${id}"> </div>`)
+            getPhoto(id, target)
+            target.classList.add('open')
+        }
+
+        else if (target.classList.contains('album_title') && target.classList.contains('open') ) {
+            let deleteTarget = document.querySelector(`.album__content_${id}`)
+            deleteTarget.remove()
+            target.classList.remove('open')
+        }
+    })
+}
+
+function getFavourites() {
+    btnCatalog.classList.remove('btn_active')
+    btnFavorites.classList.add('btn_active')
+    mainContent.innerHTML = ''
+
+}
 
 
 //////////////////////////////////////////
 async function getUsers() {
+
     try {
         displayPreLoader(mainContent)
         let users = await fetch(usersUrl)
@@ -32,7 +88,7 @@ async function getUsers() {
         displayError(mainContent)
     }
     finally {
-        removePreLoader()
+        removePreLoader(mainContent)
     }
         // return users
 }
@@ -40,7 +96,7 @@ async function getUsers() {
 async function getAlbums(userID, target) {
 
     try {
-        // displayPreLoader(target)
+         displayPreLoader(target)
         let mainItem = document.querySelector(`.item__content_${userID}`)
         let albums = await fetch(albumsUrl + userID)
             .then(response => response.json())
@@ -55,7 +111,7 @@ async function getAlbums(userID, target) {
         displayError(target)
     }
     finally {
-        // removePreLoader()
+         removePreLoader()
 
     }
 
@@ -66,7 +122,7 @@ async function getAlbums(userID, target) {
 async function getPhoto(albumID, target) {
 
     try {
-        // displayPreLoader(target)
+         displayPreLoader(target)
         let mainItem = document.querySelector(`.album__content_${albumID}`)
         let photo = await fetch(photoUrl + albumID)
             .then(response => response.json())
@@ -81,7 +137,7 @@ async function getPhoto(albumID, target) {
         displayError(target)
     }
     finally {
-        // removePreLoader()
+         removePreLoader()
     }
 
 
@@ -96,9 +152,11 @@ function displayUsers(item) {
     mainContent.insertAdjacentHTML('beforeend',
     `
     <div class="main__item" id="${id}">
-        <button class="button_plus_minus" id="${id}"> + </button>   
-        <div class="item_title" id="${id}"> 
-        ${username}
+        <div class="item__box">
+            <button class="button_plus_minus" id="${id}"> + </button>   
+            <div class="item_title" id="${id}"> 
+            ${username}
+            </div>
         </div>
             
     </div>
@@ -113,9 +171,11 @@ function displayAlbums(item, mainItem) {
     mainItem.insertAdjacentHTML('beforeend',
         `
     <div class="main__album" id="${id}">
-        <button class="button_plus_minus"> + </button>
-        <div class="album_title" id="${id}"> 
-        ${title}
+        <div class="album__box">
+             <button class="button_plus_minus"> + </button>
+            <div class="album_title" id="${id}">
+            ${title}
+            </div>
         </div>
     </div>
         `)
@@ -142,7 +202,7 @@ function displayPreLoader(target) {
 
     target.insertAdjacentHTML('afterend',
         `
-        <div class="pre-loader">
+        <div class="preloader">
             <img src="${preLoader}"
         </div>
         `)
@@ -160,7 +220,7 @@ function displayError(target) {
 }
 
 function removePreLoader() {
-    let preLoader = document.querySelector('.pre-loader')
+    let preLoader = document.querySelector('.preloader')
     preLoader.remove()
 
 }
@@ -182,46 +242,8 @@ function removeError() {
 // }
 
 
-mainContent.addEventListener('click', function (event) {
-    let target = event.target
-    let id = target.getAttribute('id')
 
-    if (target.classList.contains('item_title') && !target.classList.contains('open') ) {
-        target.insertAdjacentHTML('afterend', `<div class="item__content item__content_${id}"> </div>`)
-        getAlbums(id, target)
-        target.classList.add('open')
-    }
 
-    else if (target.classList.contains('item_title') && target.classList.contains('open') ) {
-        let deleteTarget = document.querySelector(`.item__content_${id}`)
-        deleteTarget.remove()
-        target.classList.remove('open')
-    }
 
-    else if (target.classList.contains('album_title') && !target.classList.contains('open')) {
-        target.insertAdjacentHTML('afterend', `<div class="album__content album__content_${id}"> </div>`)
-        getPhoto(id, target)
-        target.classList.add('open')
-    }
 
-    else if (target.classList.contains('album_title') && target.classList.contains('open') ) {
-        let deleteTarget = document.querySelector(`.album__content_${id}`)
-        deleteTarget.remove()
-        target.classList.remove('open')
-    }
-})
 
-//Добавление слушателя каждому элементу списка
-// function addModEvent() {
-//     let mod = document.querySelectorAll('.main__item');
-//     for (let i = 0; i < mod.length; i++) {
-//         mod[i].addEventListener('click', clickUser);
-//     }
-// }
-
-// addModEvent();
-
-// document.querySelector('body').addEventListener('click', function(event) {
-//     if (event.target.tagName.toLowerCase() === 'li') {
-//         // do your action on your 'li' or whatever it is you're listening for
-//     } БОЛЕЕ ГИБКОЕ РЕШЕНИЕ, НАДО ПОНЯТЬ, КАК РЕАЛИЗОВАТЬ
